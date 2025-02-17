25d72d676ac8a51405e0b7b4d3dedf8d03c0190f
prasanth.g@work.normsoftware.com <prasanth.g@work.normsoftware.com>
2025-02-17 05:58:00 -0500

    Intial commit

---
+++ SecureToDoApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SecureToDoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SecureToDoApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SecureToDoApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
